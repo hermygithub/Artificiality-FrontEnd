2fc6e02dbdd07cdeab50f5c526efc89f0d2bf9f5
Herman Vereycken <hermygithub@users.noreply.github.com>
2020-05-21 23:25:19 +0100

    Add files via upload

---
+++ jscripts/init-firebase-auth-config.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+  apiKey: "${API-KEY}$",
+  projectId: "${PROJECT-ID}$",
+  appId: "${APP-ID}$",
+  authDomain: "${APP-ID}$.firebaseapp.com",
+//  databaseURL: "${FIREBASE-DATABASE-URL}$",
+//  storageBucket: "${STORAGEBUCKET-URL}$",
+//  messagingSenderId: "${SENDER-ID}$",
+//  measurementId: "${MEASUREMENT-ID}$",
+};
